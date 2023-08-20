c08deb0fd1239668151a1ed26dbece07a4fe12e1
Suraj Poddar <surajku2022@gmail.com>
2023-08-20 13:12:20 +0530

    bugs fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,34 +7,37 @@ const Navbar = ({ burger, setBurger }) => {
         burger ? "navbar_active" : "navbar_inactive"
       }`}
     >
-      <li className="hover:bg-sky-700 mr-8 p-2 cursor-pointer">
-        <a href="#book" onClick={() => setBurger(!burger)}>
-          Book
-        </a>
+      <li
+        className="hover:bg-sky-700 mr-8 p-2 cursor-pointer"
+        onClick={() => setBurger(!burger)}
+      >
+        <a href="#book">Book</a>
       </li>
-      <li className="hover:bg-sky-700 mr-8 p-2 cursor-pointer">
-        <a href="#author" onClick={() => setBurger(!burger)}>
-          Author
-        </a>
+      <li
+        className="hover:bg-sky-700 mr-8 p-2 cursor-pointer"
+        onClick={() => setBurger(!burger)}
+      >
+        <a href="#author">Author</a>
       </li>
-      <li className="hover:bg-sky-700 mr-8 p-2 cursor-pointer">
-        <a href="#programs" onClick={() => setBurger(!burger)}>
-          Programs
-        </a>
+      <li
+        className="hover:bg-sky-700 mr-8 p-2 cursor-pointer"
+        onClick={() => setBurger(!burger)}
+      >
+        <a href="#programs">Programs</a>
       </li>
-      <li className="hover:bg-sky-700 mr-8 p-2 cursor-pointer">
-        <a
-          href="https://www.amazon.in/gp/aw/review/B0CCJDM7KK/R2NFLGFC6DEPUD?ref_=cm_sw_r_apin_dprv_CJMVMX39T52E45DN5V98"
-          onClick={() => setBurger(!burger)}
-        >
+      <li
+        className="hover:bg-sky-700 mr-8 p-2 cursor-pointer"
+        onClick={() => setBurger(!burger)}
+      >
+        <a href="https://www.amazon.in/gp/aw/review/B0CCJDM7KK/R2NFLGFC6DEPUD?ref_=cm_sw_r_apin_dprv_CJMVMX39T52E45DN5V98">
           Reviews
         </a>
       </li>
-      <li className="hover:bg-sky-700 mr-8 p-2 cursor-pointer">
-        <a
-          href="https://www.amazon.in/dp/B0CCJDM7KK?ref_=cm_sw_r_cp_ud_dp_A8P1019JDY2B0AMZSQ3M"
-          onClick={() => setBurger(!burger)}
-        >
+      <li
+        className="hover:bg-sky-700 mr-8 p-2 cursor-pointer"
+        onClick={() => setBurger(!burger)}
+      >
+        <a href="https://www.amazon.in/dp/B0CCJDM7KK?ref_=cm_sw_r_cp_ud_dp_A8P1019JDY2B0AMZSQ3M">
           Buy
         </a>
       </li>
